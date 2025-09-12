d948d4a107821e36ed3640244fce54adadff11e5
Izak Mohamed <71826622+IZAK-M@users.noreply.github.com>
2025-09-12 23:12:09 +0200

    more folder

---
+++ scripts/export_tu.py
@@ -0,0 +1,30 @@
+import os 
+from dotenv import load_dotenv
+from google.transit import gtfs_realtime_pb2
+import requests
+
+# Chargement des variables d'environnement
+load_dotenv()
+url = os.getenv("GTFS_RT_TU_URL")
+
+if not url:
+    raise ValueError("⚠️ Variable d'environnement GTFS_RT_TU_URL non définie.")
+# Télechargement des données
+response = requests.get(url, timeout=30)
+
+# Parser le flux 
+feed = gtfs_realtime_pb2.FeedMessage()
+feed.ParseFromString(response.content)
+
+# Création du dossier d'export
+os.makedirs("exports", exist_ok = True)
+out_path = os.path.join("exports", "trip_updates.txt")
+
+# Écriture des données
+with open(out_path, "w", encoding="utf-8") as f:
+    for entity in feed.entity:
+        if entity.HasField("trip_update"):
+            f.write(str(entity.trip_update))
+            f.write("\n") # pour séparer les entités entre eux
+
+print(f"✅ Exportation terminée : {out_path}")
